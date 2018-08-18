394be997d35cdb32d3db76a34ecdc8559c179d57
estebanelm <estebanedcv@gmail.com>
2018-08-18 15:05:07 -0600

    Agregando proyecto de netbeans

---
+++ Proyecto1/main.cpp
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/* 
+ * File:   main.cpp
+ * Author: pelo
+ *
+ * Created on August 18, 2018, 3:01 PM
+ */
+
+#include <cstdlib>
+#include <cstdio>
+
+using namespace std;
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+    printf("Hello test\n");
+    return 0;
+}
+
